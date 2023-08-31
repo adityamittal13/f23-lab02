219fa57c2f4139df655c5858399c2f030c6e4fa8
Aditya <adityamittal307@gmail.com>
2023-08-31 17:34:35 -0400

    changed java implementation to include encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,10 +1,13 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        final int height = 2;
+        final int width = 3;
+        Shape rectangle = new Rectangle(height, width);
 
         Renderer renderer = new Renderer(rectangle);
 
